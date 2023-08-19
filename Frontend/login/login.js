584a68ad0ed997b375a31e1a190a65a11286a292
joinabhi <abhi.gpt96@gmail.com>
2023-08-19 12:16:09 +0530

    add frontend of chatApp

--- Frontend/login/login.js
+++ Frontend/login/login.js
@@ -10,18 +10,18 @@ async function login(event) {
         }
         document.forms[0].reset();
 
-        const response = axios.post("http://localhost:5100/user/add-signIn", userLogin)
+        const response = await axios.post("http://localhost:5100/user/add-signIn", userLogin)
         console.log('16------16----------16', response)
         if (response.status === 201) {
             alert(response.data.message)
 
-            console.log('166666666666666', token)
+            console.log('166666666666666', response.data.token)
             localStorage.setItem('token', response.data.token)
-            //    window.location.href="../expenseTracker/expense.html"
+               window.location.href="../chatApp/chatApp.html"
         }
     }catch(error) {
-            console.log(JSON.stringify(err))
-            document.body.innerHTML += `<div style="color:red;">${err.message}</div>`
+            console.log(JSON.stringify(error))
+            document.body.innerHTML += `<div style="color:red;">${error.message}</div>`
         }
 
     }   
